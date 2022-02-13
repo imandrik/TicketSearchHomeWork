df22b9f3b32a9a4640d2519ad7b0404362831da5
Igor Mandrik <creation@86@mail.ru>
2022-02-13 14:07:36 +0800

    Updated pom.xml and added Ticket.java

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Ticket implements Comparable<Ticket> {
+    private int id;
+    private int price;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int flightDuration;
+
+    @Override
+    public int compareTo(Ticket o) {
+        return this.price - o.price;
+    }
+}
